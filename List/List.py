cc9ae546475959e1378819410c0a73611e591b1e
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-09-20 10:44:34 +0530

    LIST1

---
+++ List/List.py
@@ -0,0 +1,27 @@
+shoppingList = ['Milk','Cheese','Butter']
+print(shoppingList[0])
+print(shoppingList[1])
+print(shoppingList[2])
+
+#PRINT IN REVERSE ORDER
+print("#########################")
+print(shoppingList[-1])
+print(shoppingList[-2])
+print(shoppingList[-3])
+
+#FIND
+print("#########################")
+print('Milk' in shoppingList)
+print('milk' in shoppingList)
+
+#TRAVERSE
+print("#########################")
+def travese(list):
+    for i in list:
+        print(i)
+travese(shoppingList)
+
+#UPDATE
+print("#########################")
+shoppingList[2] = 'COFFEE'
+print(shoppingList)
